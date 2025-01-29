ac434016ac8c96548b2d7b483b23cf634d09257f
Isak Folke <isvack@gmail.com>
2025-01-29 15:17:04 +0100

    Initial commit

---
+++ src/main/java/org/example/credentials/FileEnvCredentials.java
@@ -0,0 +1,51 @@
+package org.example.credentials;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class FileEnvCredentials implements DatabaseCredentials {
+
+    private static String dbUrl, username;
+
+    private static FileEnvCredentials instance;
+
+    private FileEnvCredentials() {
+        readProperties();
+    }
+
+    public static FileEnvCredentials getInstance() {
+        if (instance == null) {
+            instance = new FileEnvCredentials();
+        }
+        return instance;
+    }
+
+    private static void readProperties() {
+        try (FileInputStream fileInputStream = new FileInputStream("src/main/java/secret/secret.properties")) {
+            Properties properties = new Properties();
+            properties.load(fileInputStream);
+            dbUrl = properties.getProperty("db.url");
+            username = properties.getProperty("db.user");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+    @Override
+    public String getDatabaseUrl() {
+        return dbUrl;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String getPassword() {
+        return System.getenv("pumper");
+    }
+}
